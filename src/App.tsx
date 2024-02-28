e9fc55eaaa7c05da232f95053417b9c934e5f802
GitGud1234 <bgelmis@gmail.com>
2024-02-28 14:56:08 +0000

    MuiCheckbox complete

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { MuiButton } from './components/MuiButton';
 import { MuiTextField } from './components/MuiTextField';
 import { MuiSelect } from './components/MuiSelect';
 import { MuiRadioButton } from './components/MuiRadioButton';
+import { MuiCheckbox } from './components/MuiCheckbox';
 
 /*
 const theme = createTheme({
@@ -32,7 +33,8 @@ function App() {
       {/* {<MuiButton />} */}
       {/* {<MuiTextField />} */}
       {/* {<MuiSelect />} */}
-      {<MuiRadioButton />}
+      {/* {<MuiRadioButton />} */}
+      {<MuiCheckbox />}
     </div>
     //</ThemeProvider>
   );
